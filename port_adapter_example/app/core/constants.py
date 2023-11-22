5cf2c5b858e2ca18548a46c636f5c9ae8cfa7043
TDEMARES <timothee.demares@ingeliance.com>
2023-11-22 14:30:42 +0100

    stask

--- port_adapter_example/app/core/constants.py
+++ port_adapter_example/app/core/constants.py
@@ -1,6 +1,6 @@
 """Constantes de l'application."""
 from pathlib import Path
 
-NASTRAN_FILE_PATH = Path(r"D:\TDEMARES\port_adapter_example\lib_ext\nastran\modele.bdf")
+NASTRAN_FILE_PATH = Path(r"lib_ext\nastran\modele.bdf")
 
-ABAQUS_FILE_PATH = Path(r"D:\TDEMARES\port_adapter_example\lib_ext\abaqus\modele.inp")
+ABAQUS_FILE_PATH = Path(r"lib_ext\abaqus\modele.inp")
